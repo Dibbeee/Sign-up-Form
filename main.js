2a341d922f17b1c3014ef8469d2e63792e6d09e6
joeldiberius <joeldiberius@gmail.com>
2023-04-18 22:41:01 +0200

    figuring out matching pw with JS

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let submitBtn = document.getElementById("submitBtn");
+submitBtn = document.addEventListener('click', checkPassword());
+
+// function checkPassword() {
+//     let password = document.getElementById("password").value;
+//     let confirmPassword = document.getElementById("confirmPassword").value;
+//     if (password.value === confirmPassword.value) {
+//         console.log("Your passwords match");
+//     } else{
+//         console.log('Password not matched')
+//     }
+// }
