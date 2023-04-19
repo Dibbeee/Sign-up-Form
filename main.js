d50d37020a39bddd3ae4f3298c9648334d47e32c
joeldiberius <joeldiberius@gmail.com>
2023-04-19 09:56:06 +0200

    finished the with mockup with some JS :)

--- main.js
+++ main.js
@@ -2,16 +2,20 @@
 document.querySelector('.submitBtn').onclick = function() {
     const password = document.querySelector('.password').value;
     const confirmPassword = document.querySelector('.confirmPassword').value;
+    const validatePass = document.querySelector('.validatePass');
+    const validateConfirmPass = document.querySelector('.validateConfirmPass');
+    
 
     if(password === ''){
         alert('Please enter your password');
     }else if(confirmPassword === ''){
-        alert('Please confirm your password');
+        alert('Please confirm your password.');
     }else if(password!== confirmPassword){
-        alert('Passwords do not match');
+        validatePass.textContent = 'Passwords do not match!';
+        validateConfirmPass.textContent = 'Passwords do not match!';
         return false;
     }else if(password === confirmPassword){
-        alert('Passwords match');
+        alert('Passwords match.');
         return true;
     }
 }
